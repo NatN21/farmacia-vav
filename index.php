48a6a5f36671f151b6466098df38b5f0a48f4a27
duda <mariaeduardaporodrigues@gmail.com>
2026-05-14 18:36:10 -0300

    inicio do index php

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-require_once 'includes/header.php';
+require 'conexao.php';
+require 'header.php';
+
+// SELECT dos produtos
+$sql = "SELECT * FROM produtos ORDER BY nome ASC";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+// fetchAll()
+$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<h2>Bem-vindo ao sistema!</h2>
-
-<?php
-require_once 'includes/footer.php';
-?>
