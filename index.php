5d1d1749467526cf61c9efb196d338bb0598c045
duda <mariaeduardaporodrigues@gmail.com>
2026-05-14 18:36:36 -0300

    Remove comments from index.php

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 require 'conexao.php';
 require 'header.php';
 
-// SELECT dos produtos
 $sql = "SELECT * FROM produtos ORDER BY nome ASC";
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 
-// fetchAll()
 $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
