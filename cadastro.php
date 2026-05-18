826fe4d73e1da1a7e07548f5ff9bc02ad09e4e2d
Pietro102821 <pietro.fiorese.dopp0701@gmail.com>
2026-05-18 09:44:17 -0300

    Conexão do Banco de Dados farmacia-vav

---
+++ cadastro.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once './config/conexao.php';
+require_once './includes/header.php';
+?>
+
+<form>
+    <h1>Formulario de cadastro</h1>
+</form>
+
+<?php
+require_once './includes/footer.php';
+?>
