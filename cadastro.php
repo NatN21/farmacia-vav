6b2d358307ae301498045d696453787f716b579d
Pietro102821 <pietro.fiorese.dopp0701@gmail.com>
2026-05-18 09:47:26 -0300

    Implementação dos itens para o Banco de Dados

--- cadastro.php
+++ cadastro.php
@@ -2,12 +2,37 @@
 
 require_once './config/conexao.php';
 require_once './includes/header.php';
+
+if ($_POST) {
+
+    $nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
+    $sql = "INSERT INTO produtos 
+    (nome, fabricante, preco, estoque)
+    VALUES
+    (:nome, :fabricante, :preco, :estoque)";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        ':nome' => $nome,
+        ':fabricante' => $fabricante,
+        ':preco' => $preco,
+        ':estoque' => $estoque
+    ]);
+
+    echo "Produto cadastrado com sucesso!";
+}
 ?>
 
 <form>
-    <h1>Formulario de cadastro</h1>
+    <h1>Formulario de Cadastro</h1>
 </form>
 
 <?php
+
 require_once './includes/footer.php';
 ?>
